47e973b2c98c66ac88a1c490f0cf40637d3d1e89
Santhini sasidharan <santhi.code@gmail.com>
2022-07-22 08:33:46 -0400

    Update database.php

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,10 +1,6 @@
 <?php
 
 $connect = mysqli_connect( 
-    "sql108.epizy.com", 
-    "epiz_31881534", 
-    "DPsE0l7U2oPX3pK", 
-    "epiz_31881534_portfolio" 
 );
 
 mysqli_set_charset( $connect, 'UTF8' );
